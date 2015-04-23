9d06d13e1665e6182aeb97d50df98a914ed9f7a0
Shawn Wilkinson <me@super3.org>
2015-04-23 01:28:21 -0400

    Starting Commit

---
+++ payserv/app.py
@@ -0,0 +1,11 @@
+import sqlite3
+from flask import Flask
+
+
+# Initialize the Flask application
+app = Flask(__name__)
+app.config['DATABASE'] = '/db/payserv.db'
+
+# Database code
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])

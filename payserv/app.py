f6ffc40d8ed7b7d13d01e56fe0dc1ff3e81b96f7
Shawn Wilkinson <me@super3.org>
2015-05-14 23:33:57 -0400

    Hello World

--- payserv/app.py
+++ payserv/app.py
@@ -10,3 +10,19 @@ app.config['DATABASE'] = '/db/payserv.db'
 # Database code
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+
+# Routes
+@app.route('/')
+def index():
+    return "Hello World"
+
+
+# Run application
+if __name__ == '__main__':
+    # Run the Flask app
+    app.run(
+        host="0.0.0.0",
+        port=int("5000"),
+        debug=True
+    )
